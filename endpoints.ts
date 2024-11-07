b389f2289fddd7d3f3ddd4a3c6ae48c3d35cc076
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-07 15:11:34 +0100

    extracted endpoints here

---
+++ endpoints.ts
@@ -0,0 +1,4 @@
+export const restCountriesEndpoint = 'https://restcountries.com/v3.1/region/europe';
+export const edamamBaseUrl = 'https://api.edamam.com/api/recipes/v2?type=public';
+export const locationiqBaseUrl = 'https://api.locationiq.com/v1/autocomplete';
+export const fakestoreBaseUrl = 'https://fakestoreapi.com/products/category/';
