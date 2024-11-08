94ee233ed29c8540bde1d16fb7c2a8436b2fde19
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-08 13:23:34 +0100

    work

--- composables/useFirebaseActions.ts
+++ composables/useFirebaseActions.ts
@@ -1,22 +1,39 @@
-import { getDatabase, ref, set, push } from 'firebase/database';
+import { getDatabase, ref, set, get, remove } from 'firebase/database';
 
 import type { IItem } from '@/interfaces/interfaces.interface';
 
 declare module '#app' {
   interface NuxtApp {
-    $database: ReturnType<typeof getDatabase>; // Correctly type $database
+    $database: ReturnType<typeof getDatabase>;
   }
 }
 
 export function useFirebaseActions() {
   const { user } = useAuth();
+  const { $database } = useNuxtApp();
 
-  const { $auth, $database } = useNuxtApp();
-
-  function writeFavoriteUserItemData(favoriteItem: IItem) {
+  async function writeFavoriteUserItemData(favoriteItem: IItem) {
     const favoriteItemRef = ref($database, 'users/' + user.value?.uid + '/favoriteItems');
-    const newFavoriteItemRef = push(favoriteItemRef);
-    return set(newFavoriteItemRef, favoriteItem);
+    const snapshot = await get(favoriteItemRef);
+    const isFavorite = snapshot.exists();
+    /*  const newFavoriteItemRef = push(favoriteItemRef);
+
+    return set(newFavoriteItemRef, favoriteItem); */
+
+    if (isFavorite) {
+      await remove(favoriteItemRef);
+    } else {
+      await set(favoriteItemRef, favoriteItem);
+    }
   }
-  return { writeFavoriteUserItemData } as const;
+
+  async function fetchFavoriteStatus(favoriteItem: IItem): Promise<boolean> {
+    const favoriteItemRef = ref(
+      $database,
+      `users/${user.value?.uid}/favoriteItems/${favoriteItem.id}`
+    );
+    const snapshot = await get(favoriteItemRef);
+    return snapshot.exists();
+  }
+  return { writeFavoriteUserItemData, fetchFavoriteStatus } as const;
 }
