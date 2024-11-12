2be630920cfc38bbd948beb3f782f44c6da07aa9
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-12 14:53:32 +0100

    added interface

--- components/ui/GenericList/GenericListItem.vue
+++ components/ui/GenericList/GenericListItem.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
 import { capitalizeFirstLetter } from '@/helpers/capitalizeFirstLetter';
-import type { IItem } from '@/interfaces/interfaces.interface';
+import type { GenericListProps } from '@/interfaces/interfaces.interface';
 
-const props = defineProps({
-  items: {
-    type: Array as PropType<IItem[]>,
-    default: () => [],
-  },
-  field: {
-    type: String,
-    default: '',
-  },
-});
+const props = defineProps<{
+  items: Array<Record<string, any>>;
+  field: string;
+}>();
 
-function getItemField(item: IItem, field: string) {
-  const nestedFields = field.split('.'); // Split the field by '.' to handle nested properties
+function getItemField(item: Record<string, any>, field: string): string {
+  const nestedFields = field.split('.');
 
   let value = item;
 
+  // Traverse the object based on the nested fields
   for (const nestedField of nestedFields) {
-    if (value === null || value === undefined) return ''; // Handle null or undefined values
+    if (value == null) return '';
     value = value[nestedField];
   }
   return value != null ? String(value) : '';
@@ -27,11 +22,13 @@ function getItemField(item: IItem, field: string) {
 </script>
 
 <template>
-  <li
-    class="bg-orange-200 text-lg font-semibold p-4 rounded-3xl cursor-pointer"
-    v-for="(item, key) in props.items"
-    :key="key"
-  >
-    {{ capitalizeFirstLetter(getItemField(item, props.field || '')) }}
-  </li>
+  <ul class="flex flex-wrap gap-2 justify-around">
+    <li
+      v-for="(item, key) in props.items"
+      :key="key"
+      class="bg-orange-200 text-lg font-semibold p-4 rounded-3xl cursor-pointer"
+    >
+      {{ capitalizeFirstLetter(getItemField(item, props.field || '')) }}
+    </li>
+  </ul>
 </template>
