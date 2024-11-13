40fbf0ac0d9f5c7dfdd518a5bb8a1c5e47055940
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 12:49:39 +0100

    added dashboard

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -3,6 +3,6 @@ import type { INavLinks } from '@/interfaces/interfaces.interface';
 export const navLinks: INavLinks[] = [
   { title: 'Home', link: '', icon: ['fas', 'house'] },
   { title: 'Cart', link: '', icon: ['fas', 'cart-shopping'] },
-  { title: 'My Orders', link: '', icon: ['fas', 'newspaper'] },
+  { title: 'Dashboard', link: '', icon: ['fas', 'user-tie'] },
   { title: 'My Account', link: '', icon: ['fas', 'face-smile'] },
 ];
