1dfce80f4b073ecd89b9cc68a4304685742ae412
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 14:04:55 +0100

    hid cartCounter on smallest screens

--- components/header/links/NavLink.vue
+++ components/header/links/NavLink.vue
@@ -35,7 +35,7 @@ const shouldShowNavItems = computed(() => {
     </div>
     <p
       v-if="props.title === 'Cart' && shouldShowNavItems"
-      class="ml-1 bg-green-600 text-white rounded-full p-2 w-6 h-6 flex items-center justify-center text-xs font-semibold"
+      class="hidden ml-1 bg-green-600 text-white rounded-full p-2 w-6 h-6 sm:flex items-center justify-center text-xs font-semibold"
     >
       {{ numberOfCartProducts }}
     </p>
