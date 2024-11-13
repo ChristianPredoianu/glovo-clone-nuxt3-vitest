7a71a54e1d761bc064d73476f307fddc1535f0b1
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 13:36:00 +0100

    added links

--- components/header/links/NavLink.vue
+++ components/header/links/NavLink.vue
@@ -8,18 +8,25 @@ const props = defineProps({
     type: Array as () => string[],
   },
 });
+
+const shouldShowNavItems = computed(() => {
+  return (
+    !(screenWidth.value > 768 && props.title === 'Cart') &&
+    (screenWidth.value < 640 || props.title !== 'Sign in')
+  );
+});
 </script>
 
 <template>
   <li
-    v-if="!(screenWidth > 640 && props.title === 'Cart')"
-    class="block text-2xl font-semibold text-gray-600 sm:text-sky-900 sm:text-sm sm:inline-block text-teal-lighter sm:hover:text-white cursor-pointer sm:mr-4"
+    v-if="shouldShowNavItems"
+    class="block text-2xl font-semibold text-gray-700 sm:text-sm sm:inline-block text-teal-lighter sm:hover:text-white cursor-pointer sm:mr-4"
     :class="{
       'text-xs': screenWidth < 230,
     }"
   >
     <div
-      class="w-full flex items-center justify-between border-b-2 sm:border-0 py-2 cursor-pointer"
+      class="w-full flex items-center justify-between border-b-2 sm:mt-1 sm:border-0 py-2 cursor-pointer"
     >
       {{ props.title }}
       <font-awesome-icon :icon="props.icon" v-if="screenWidth < 640" />
