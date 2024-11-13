646f47e40880ebd68d39376a5b8fd2b97cc5b793
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 14:01:09 +0100

    changed order

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -2,8 +2,8 @@ import type { INavLinks } from '@/interfaces/interfaces.interface';
 
 export const navLinks: INavLinks[] = [
   { title: 'Home', link: '', icon: ['fas', 'house'] },
-  { title: 'Cart', link: '', icon: ['fas', 'cart-shopping'] },
   { title: 'Dashboard', link: '', icon: ['fas', 'user-tie'] },
   { title: 'My Account', link: '', icon: ['fas', 'face-smile'] },
+  { title: 'Cart', link: '', icon: ['fas', 'cart-shopping'] },
   { title: 'Sign in', link: '', icon: ['fas', 'right-to-bracket'] },
 ];
