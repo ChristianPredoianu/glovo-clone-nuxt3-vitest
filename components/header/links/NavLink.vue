58cddafd3d25678cda8f499565de63932a1a5bfe
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 13:59:43 +0100

    fixed nav  links , the cart

--- components/header/links/NavLink.vue
+++ components/header/links/NavLink.vue
@@ -9,6 +9,8 @@ const props = defineProps({
   },
 });
 
+const { numberOfCartProducts } = useCart();
+
 const shouldShowNavItems = computed(() => {
   return (
     !(screenWidth.value > 768 && props.title === 'Cart') &&
@@ -20,7 +22,7 @@ const shouldShowNavItems = computed(() => {
 <template>
   <li
     v-if="shouldShowNavItems"
-    class="block text-2xl font-semibold text-gray-700 sm:text-sm sm:inline-block text-teal-lighter sm:hover:text-white cursor-pointer sm:mr-4"
+    class="block sm:flex items-center text-2xl font-semibold text-gray-700 sm:text-sm text-teal-lighter sm:hover:text-white cursor-pointer sm:mr-4"
     :class="{
       'text-xs': screenWidth < 230,
     }"
@@ -31,5 +33,11 @@ const shouldShowNavItems = computed(() => {
       {{ props.title }}
       <font-awesome-icon :icon="props.icon" v-if="screenWidth < 640" />
     </div>
+    <p
+      v-if="props.title === 'Cart' && shouldShowNavItems"
+      class="ml-1 bg-green-500 text-white rounded-full p-2 w-6 h-6 flex items-center justify-center text-xs font-semibold"
+    >
+      {{ numberOfCartProducts }}
+    </p>
   </li>
 </template>
