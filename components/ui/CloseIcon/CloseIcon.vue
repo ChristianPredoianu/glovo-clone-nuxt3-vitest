db352c71507ed8d3682a44e71428262e57aa8718
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 11:21:18 +0100

    added folder

---
+++ components/ui/CloseIcon/CloseIcon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emits = defineEmits(['close']);
+</script>
+
+<template>
+  <font-awesome-icon
+    :icon="['fas', 'close']"
+    class="text-2xl text-right mb-8 font-semibold cursor-pointer"
+    @click="emits('close')"
+  />
+</template>
