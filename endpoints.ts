c3900f7b61ede361f4578ab6a7ee57529767bf78
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-13 12:21:42 +0100

    added base endpoint

--- endpoints.ts
+++ endpoints.ts
@@ -1,4 +1,5 @@
 export const restCountriesEndpoint = 'https://restcountries.com/v3.1/region/europe';
 export const edamamBaseUrl = 'https://api.edamam.com/api/recipes/v2?type=public';
 export const locationiqBaseUrl = 'https://api.locationiq.com/v1/autocomplete';
+export const locationiqReverse = 'https://us1.locationiq.com/v1/reverse';
 export const fakestoreBaseUrl = 'https://fakestoreapi.com/products/category/';
