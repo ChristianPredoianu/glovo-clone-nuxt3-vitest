e25cbeb453b468829daaaadac2f3af56e4416df4
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-14 12:43:50 +0100

    removed openModal

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { user } = useAuth();
-  const { openModal } = useModal();
 
   if (!user.value) {
-    openModal('productModal');
     return navigateTo('/');
   }
 });
