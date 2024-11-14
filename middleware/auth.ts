4d334ed459828394d6ddb06a27d6c40266044a5a
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-14 13:33:17 +0100

    fixed middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,11 +3,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
   if (!isAuthReady.value) return; // Don't run the middleware until Firebase is ready
 
-  // If the user is not authenticated, redirect them to the login page
   if (!user.value) {
     return navigateTo({
       path: '/',
-      query: { redirect: to.fullPath }, // Save the intended route for after login
+      query: { redirect: to.fullPath },
     });
   }
 });
