c5e94049c5b77545819e228fcd07397b2a1c85ed
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-18 12:54:35 +0100

    added data-test to wont awesome icon

--- components/buttons/RoundedBtn.vue
+++ components/buttons/RoundedBtn.vue
@@ -9,13 +9,14 @@ const emits = defineEmits(['emitClick']);
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-1">
+  <button class="flex flex-col gap-y-1">
     <font-awesome-icon
       :icon="['fas', props.icon]"
+      data-test="fa-icon"
       class="bg-orange-200 p-2 rounded-full text-gray-600 cursor-pointer h-4 w-4"
       :class="[props.backCol]"
       @click="emits('emitClick')"
     />
     <p class="text-center font-semibold">{{ props.text }}</p>
-  </div>
+  </button>
 </template>
