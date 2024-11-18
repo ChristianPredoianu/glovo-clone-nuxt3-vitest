fcc5c19711a4be8c7a0003a2f2c56236eb4b519e
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-18 17:44:58 +0100

    correct name for describe

--- components/buttons/RoundedBtn/RoundedBtn.nuxt.test.ts
+++ components/buttons/RoundedBtn/RoundedBtn.nuxt.test.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import { describe, it, expect, vi } from 'vitest';
 import RoundedBtn from '@/components/buttons/RoundedBtn/RoundedBtn.vue';
 
-describe('RoundedBtn.vue', () => {
+describe('RoundedBtn', () => {
   it('renders the correct icon and text based on props', () => {
     const wrapper = mount(RoundedBtn, {
       props: {
