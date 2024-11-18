d35f8ec94e993ab3b2ad664492ad00e2eb6ca71b
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-18 18:43:35 +0100

    added data-test to elements

--- components/counters/ProductCounter/ProductCounter.vue
+++ components/counters/ProductCounter/ProductCounter.vue
@@ -12,13 +12,15 @@ const { addToCart, removeFromCart } = useCart();
 <template>
   <div class="flex space-between items-center justify-center gap-2">
     <p
+      data-test="remove-btn"
       class="bg-gray-500 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8 cursor-pointer"
       @click="removeFromCart(product.id)"
     >
       -
     </p>
-    <p class="border border-gray-500 py-1 px-3">{{ quantity }}</p>
+    <p data-test="quantity" class="border border-gray-500 py-1 px-3">{{ quantity }}</p>
     <p
+      data-test="add-btn"
       class="bg-gray-400 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8 cursor-pointer"
       @click="addToCart(product)"
     >
