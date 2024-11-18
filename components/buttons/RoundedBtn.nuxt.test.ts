c3aa3e940408c8da568d02e62d46d2a818fd4df3
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-18 12:54:50 +0100

    added test

--- components/buttons/RoundedBtn.nuxt.test.ts
+++ components/buttons/RoundedBtn.nuxt.test.ts
@@ -6,25 +6,37 @@ describe('RoundedBtn.vue', () => {
   it('renders the correct icon and text based on props', () => {
     const wrapper = mount(RoundedBtn, {
       props: {
-        text: 'Test Text',
-        icon: 'fa-check',
+        text: 'Click Me',
+        icon: 'home',
         backCol: 'bg-red-500',
-      },
-      global: {
-        stubs: {
-          FontAwesomeIcon: {
-            template: '<div :class="[$attrs.class]" :data-icon="icon"></div>',
-          }, // Custom stub for FontAwesomeIcon
-        },
       },
     });
 
-    expect(wrapper.find('p').text()).toBe('Test Text');
+    expect(wrapper.text()).toContain('Click Me');
 
-    const icon = wrapper.findComponent({ name: 'FontAwesomeIcon' });
+    const icon = wrapper.find('[data-test="fa-icon"]');
     expect(icon.exists()).toBe(true);
-    expect(icon.props('icon')).toEqual(['fas', 'fa-check']);
+    expect(icon.attributes('class')).toContain('bg-red-500');
+  });
 
-    expect(icon.classes()).toContain('bg-red-500');
+  it('emits "emitClick" when the icon is clicked', async () => {
+    const emitClickMock = vi.fn();
+
+    const wrapper = mount(RoundedBtn, {
+      props: {
+        text: 'Click Me',
+        icon: 'home',
+        backCol: 'bg-red-500',
+      },
+
+      attrs: {
+        onEmitClick: emitClickMock,
+      },
+    });
+
+    const icon = wrapper.find('[data-test="fa-icon"]');
+    await icon.trigger('click');
+
+    expect(wrapper.emitted('emitClick')).toHaveLength(1);
   });
 });
