d8acbbf559b5e0a33c50f9dd0294d5e8dea955e5
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-19 13:37:43 +0100

    refactored

--- components/cards/ProductCard/ProductCard.nuxt.test.ts
+++ components/cards/ProductCard/ProductCard.nuxt.test.ts
@@ -1,11 +1,11 @@
-import { mount } from '@vue/test-utils';
+import { mount, VueWrapper } from '@vue/test-utils';
 import { vi } from 'vitest';
 import ProductCard from '@/components/cards/ProductCard/ProductCard.vue';
 
 vi.mock('@/components/StarCounter.vue', () => ({
   name: 'StarCounter',
   props: ['rate', 'count'],
-  template: '<div class="star-counter"></div>', // A mock template to ensure it renders
+  template: '<div class="star-counter"></div>',
 }));
 
 describe('ProductCard', () => {
@@ -19,52 +19,37 @@ describe('ProductCard', () => {
     },
   };
 
-  it('renders the product image, title, and price correctly', () => {
-    const wrapper = mount(ProductCard, {
-      props: {
-        product,
-      },
-    });
+  let wrapper: VueWrapper<any>;
 
-    // Test the product image rendering
+  beforeEach(() => {
+    wrapper = mount(ProductCard, {
+      props: { product },
+    });
+  });
+
+  it('renders the product image, title, and price correctly', () => {
     const img = wrapper.find('img');
     expect(img.exists()).toBe(true);
     expect(img.attributes('src')).toBe(product.image);
-    expect(img.attributes('alt')).toBe('Product image'); // Test alt attribute
+    expect(img.attributes('alt')).toBe('Product image');
 
-    // Test the product title rendering
     const title = wrapper.find('h3');
     expect(title.exists()).toBe(true);
     expect(title.text()).toBe(product.title);
 
-    // Test the product price rendering
     const price = wrapper.find('p.text-lg');
     expect(price.exists()).toBe(true);
     expect(price.text()).toBe(`${product.price} $`);
   });
 
   it('passes correct props to the StarCounter component', () => {
-    const wrapper = mount(ProductCard, {
-      props: {
-        product,
-      },
-    });
-
     const starCounter = wrapper.findComponent({ name: 'StarCounter' });
-
-    // Ensure StarCounter receives the correct `rate` and `count` props
     expect(starCounter.exists()).toBe(true);
     expect(starCounter.props('rate')).toBe(product.rating.rate);
     expect(starCounter.props('count')).toBe(product.rating.count);
   });
 
   it('renders the "Add to cart" button', () => {
-    const wrapper = mount(ProductCard, {
-      props: {
-        product,
-      },
-    });
-
     const button = wrapper.find('button');
     expect(button.exists()).toBe(true);
     expect(button.text()).toBe('Add to cart');
