ea60503c997a2356810f2b4f3774e68dce24f1cf
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-19 11:13:40 +0100

    added data-test to p element

--- components/filters/MealCategoryFilter/MealCategoryFilterListItem.vue
+++ components/filters/MealCategoryFilter/MealCategoryFilterListItem.vue
@@ -34,7 +34,10 @@ function handleClick(selectedFilter: IFakeStoreCategories | ICuisineType) {
       class="fa-fw bg-orange-200 p-2 rounded-full text-gray-600"
       :class="{ 'bg-orange-300': isActive(getCategoryName(category)) }"
     />
-    <p :class="{ 'font-semibold': isActive(getCategoryName(category)) }">
+    <p
+      data-test="category"
+      :class="{ 'font-semibold': isActive(getCategoryName(category)) }"
+    >
       {{ getCategoryName(category) }}
     </p>
   </li>
