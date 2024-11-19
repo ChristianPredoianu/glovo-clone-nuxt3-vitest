4221862f8afa43e85cb236baf595cad9a360da04
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-19 15:39:42 +0100

    added test

---
+++ components/ui/CloseIcon/CloseIcon.nuxt.test.ts
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect } from 'vitest';
+import CloseIcon from '@/components/ui/CloseIcon/CloseIcon.vue';
+
+describe('CloseIcon.vue', () => {
+  it('emits close event when clicked', async () => {
+    const wrapper = mount(CloseIcon);
+
+    await wrapper.find('[data-test="fa-close"]').trigger('click');
+
+    expect(wrapper.emitted('close')).toBeTruthy();
+  });
+});
