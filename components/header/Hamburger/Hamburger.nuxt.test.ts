e90bf2022ff72bad9e9828cb9bfefabb0e6f56fc
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-19 15:15:09 +0100

    added etst

--- components/header/Hamburger/Hamburger.nuxt.test.ts
+++ components/header/Hamburger/Hamburger.nuxt.test.ts
@@ -2,7 +2,6 @@ import { mount } from '@vue/test-utils';
 import { describe, it, expect, vi } from 'vitest';
 import Hamburger from '@/components/header/Hamburger/Hamburger.vue';
 
-// Mock the composables
 const mockOpenNav = vi.fn();
 const mockOpenBackdrop = vi.fn();
 
@@ -22,17 +21,14 @@ describe('Hamburger.vue', () => {
   it('renders the Hamburger component', () => {
     const wrapper = mount(Hamburger);
 
-    // Check if the button is rendered
     expect(wrapper.find('button').exists()).toBe(true);
   });
 
   it('calls openNav and openBackdrop when button is clicked', async () => {
     const wrapper = mount(Hamburger);
 
-    // Simulate a click on the button
     await wrapper.find('button').trigger('click');
 
-    // Check if the mocked functions were called
     expect(mockOpenNav).toHaveBeenCalled();
     expect(mockOpenBackdrop).toHaveBeenCalled();
   });
