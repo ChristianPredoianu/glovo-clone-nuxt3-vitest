c5ea3575b9d0665244e1e14c2f48eaaf65438f4a
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-26 11:15:41 +0100

    refactored

--- components/modals/ProductModal/Overlay/ProductModalOverlay.nuxt.test.ts
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.nuxt.test.ts
@@ -17,39 +17,36 @@ vi.mock('@/composables/ui/useProgressBar', () => ({
   })),
 }));
 
-describe('ProductModalOverlay', () => {
-  const mockProductModalProps = {
-    id: '1',
-    label: 'Pizza Margherita',
-    img: 'pizza.jpg',
-    ingredients: [
-      { foodId: '1', text: 'Tomato' },
-      { foodId: '2', text: 'Cheese' },
-      { foodId: '3', text: 'Basil' },
-    ],
-  };
+const mockProductModalProps = {
+  id: '1',
+  label: 'Pizza Margherita',
+  img: 'pizza.jpg',
+  ingredients: [
+    { foodId: '1', text: 'Tomato' },
+    { foodId: '2', text: 'Cheese' },
+    { foodId: '3', text: 'Basil' },
+  ],
+};
 
-  it('renders correctly with productModalProps', () => {
-    const wrapper = mount(ProductModalOverlay, {
+describe('ProductModalOverlay', () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = mount(ProductModalOverlay, {
       props: {
         productModalProps: mockProductModalProps,
         price: 12,
       },
     });
+  });
 
+  it('renders correctly with productModalProps', () => {
     expect(wrapper.find('h3').text()).toBe('Pizza Margherita');
     expect(wrapper.find('h4').text()).toBe('12 $');
     expect(wrapper.find('img').attributes('src')).toBe('pizza.jpg');
   });
 
   it('renders ingredients list when productModalProps is a meal', () => {
-    const wrapper = mount(ProductModalOverlay, {
-      props: {
-        productModalProps: mockProductModalProps,
-        price: 12,
-      },
-    });
-
     const ingredients = wrapper.findAll('li');
     expect(ingredients.length).toBe(3);
     expect(ingredients[0].text()).toBe('Tomato');
@@ -58,29 +55,13 @@ describe('ProductModalOverlay', () => {
   });
 
   it('calls addToCart when Add to cart button is clicked', async () => {
-    const wrapper = mount(ProductModalOverlay, {
-      props: {
-        productModalProps: mockProductModalProps,
-        price: 12,
-      },
-    });
-
     const button = wrapper.findComponent({ name: 'CtaBtn' });
     await button.trigger('click');
-
     expect(addToCartMock).toHaveBeenCalled();
   });
 
   it('shows progress bar when isBarActive is true', async () => {
     isBarActiveMock.value = true;
-
-    const wrapper = mount(ProductModalOverlay, {
-      props: {
-        productModalProps: mockProductModalProps,
-        price: 12,
-      },
-    });
-
     expect(wrapper.findComponent({ name: 'ProgressBar' }).exists()).toBe(true);
   });
 });
