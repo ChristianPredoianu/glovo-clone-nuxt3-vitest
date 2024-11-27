8dcde43b7fc0fd808c000499242c66dc383fca55
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-27 13:11:09 +0100

    added data-test

--- components/ui/ProgressBar/ProgressBar.vue
+++ components/ui/ProgressBar/ProgressBar.vue
@@ -6,6 +6,7 @@ const { progressWidth } = useProgressBar();
   <div class="relative w-full h-1 bg-gray-300 rounded-md overflow-hidden">
     <!-- The progress bar filling -->
     <div
+      data-test="progress"
       class="h-full bg-green-500 transition-all duration-300 ease-linear"
       :style="{ width: progressWidth + '%' }"
     ></div>
