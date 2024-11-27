a27f430d7285323d2543048b707c53a0e01c58a2
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-27 13:08:15 +0100

    added test

--- components/ui/Profile/Profile.nuxt.test.ts
+++ components/ui/Profile/Profile.nuxt.test.ts
@@ -1,44 +1,41 @@
 import { mount } from '@vue/test-utils';
 import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { nextTick } from 'vue';
 import Profile from '@/components/ui/Profile/Profile.vue';
 
-vi.mock('@/composables/useAuth', () => ({
-  useAuth: () => ({ user: { value: { email: 'test@example.com' } } }),
+vi.mock('@/composables/auth/useAuth', () => ({
+  useAuth: () => ({
+    user: ref({ email: 'test@example.com' }),
+  }),
 }));
 
 vi.mock('@/composables/useIsLoaded', () => ({
-  default: () => ({ isLoaded: true }),
+  default: () => ({ isLoaded: ref(true) }),
 }));
 
 describe('Profile', () => {
   let wrapper: any;
 
   beforeEach(() => {
+    vi.clearAllMocks();
     wrapper = mount(Profile);
   });
 
-  it('renders user email correctly', () => {
+  it('renders user email correctly', async () => {
     const emailElement = wrapper.find('[data-test="email-element"]');
+    expect(emailElement.exists()).toBe(true);
     expect(emailElement.text()).toBe('test@example.com');
   });
 
-  /*  it('renders Admin text', () => {
+  it('renders Admin text', async () => {
+    await nextTick();
     const adminElement = wrapper.find('p.text-xs');
     expect(adminElement.text()).toBe('Admin');
-  }); */
+  });
 
-  /*  it('renders the font-awesome icon when loaded', () => {
+  it('renders the font-awesome icon when loaded', async () => {
+    await nextTick();
     const iconElement = wrapper.find('[data-test="fa-user"]');
     expect(iconElement.exists()).toBe(true);
-  }); */
-
-  /*  it('does not render the font-awesome icon when not loaded', async () => {
-    vi.mock('@/composables/useIsLoaded', () => ({
-      useIsLoaded: () => ({ isLoaded: false }),
-    }));
-
-    wrapper = mount(Profile);
-    const iconElement = wrapper.find('[data-test="fa-user"]');
-    expect(iconElement.exists()).toBe(false);
-  }); */
+  });
 });
