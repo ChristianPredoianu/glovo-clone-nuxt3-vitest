b46a508479d2a6c528a0c17b70ec74c643fcd1eb
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-27 12:18:47 +0100

    moved

---
+++ components/ui/GenericList/GenericList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { GenericListProps } from '@/interfaces/interfaces.interface';
+
+const props = defineProps<GenericListProps>();
+</script>
+
+<template>
+  <ul class="flex flex-wrap gap-2 justify-around">
+    <GenericListItem :items="items" :field="props.field" />
+  </ul>
+</template>
