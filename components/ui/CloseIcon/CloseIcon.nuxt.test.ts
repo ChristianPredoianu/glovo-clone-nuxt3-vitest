370a7be4cd9a97105444a4dc9f7c8e2272875eee
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-29 13:48:33 +0100

    removed .vue

--- components/ui/CloseIcon/CloseIcon.nuxt.test.ts
+++ components/ui/CloseIcon/CloseIcon.nuxt.test.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import { describe, it, expect } from 'vitest';
 import CloseIcon from '@/components/ui/CloseIcon/CloseIcon.vue';
 
-describe('CloseIcon.vue', () => {
+describe('CloseIcon', () => {
   it('emits close event when clicked', async () => {
     const wrapper = mount(CloseIcon);
 
