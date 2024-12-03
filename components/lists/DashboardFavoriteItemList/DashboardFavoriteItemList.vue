ee4158275e0ebb53a189214494e6b03d96f97098
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-03 13:03:10 +0100

    added list folder

---
+++ components/lists/DashboardFavoriteItemList/DashboardFavoriteItemList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul class="mt-4 space-y-4">
+    <li
+      v-for="item in displayedItems"
+      :key="item.id"
+      class="flex items-center p-4 border-2 border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 ease-in-out"
+    >
+      <img
+        src="/glovo.jpg"
+        alt="Item Image"
+        class="w-32 h-32 object-cover rounded-lg mr-4"
+      />
+      <div class="flex flex-col gap-y-1">
+        <p class="text-lg font-semibold text-gray-800">
+          {{ capitalizeFirstLetter(item.category) }}
+        </p>
+        <p class="text-sm text-gray-600">{{ replaceRecipeText(item.label) }}</p>
+      </div>
+    </li>
+  </ul>
+</template>
---
+++ components/lists/GenericList/Item/GenericListItem.nuxt.test.ts
@@ -0,0 +1,62 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect } from 'vitest';
+import GenericListItem from '@/components/ui/GenericList/Item/GenericListItem.vue';
+import { capitalizeFirstLetter } from '@/helpers/capitalizeFirstLetter';
+
+vi.mock('@/helpers/capitalizeFirstLetter', () => ({
+  capitalizeFirstLetter: vi.fn(
+    (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+  ),
+}));
+
+describe('GenericListItem', () => {
+  const items = [
+    { id: 1, name: 'item 1' },
+    { id: 2, name: 'item 2' },
+  ];
+  const field = 'name';
+
+  it('renders list items correctly', () => {
+    const wrapper = mount(GenericListItem, {
+      props: { items, field },
+    });
+
+    const listItems = wrapper.findAll('li');
+    expect(listItems).toHaveLength(items.length);
+    listItems.forEach((listItem, index) => {
+      expect(listItem.text()).toBe(capitalizeFirstLetter(items[index].name));
+    });
+  });
+
+  it('handles nested fields correctly', () => {
+    const nestedItems = [
+      { id: 1, details: { name: 'nested item 1' } },
+      { id: 2, details: { name: 'nested item 2' } },
+    ];
+    const nestedField = 'details.name';
+
+    const wrapper = mount(GenericListItem, {
+      props: { items: nestedItems, field: nestedField },
+    });
+
+    const listItems = wrapper.findAll('li');
+    expect(listItems).toHaveLength(nestedItems.length);
+    listItems.forEach((listItem, index) => {
+      expect(listItem.text()).toBe(
+        capitalizeFirstLetter(nestedItems[index].details.name)
+      );
+    });
+  });
+
+  it('returns an empty string for missing fields', () => {
+    const wrapper = mount(GenericListItem, {
+      props: { items, field: 'nonexistent' },
+    });
+
+    const listItems = wrapper.findAll('li');
+    expect(listItems).toHaveLength(items.length);
+    listItems.forEach((listItem) => {
+      expect(listItem.text()).toBe('');
+    });
+  });
+});
