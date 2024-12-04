21666a5b27153d957b4292660a4a1a0651c60737
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-04 14:59:52 +0100

    removed comment

--- components/ui/ProgressBar/ProgressBar.vue
+++ components/ui/ProgressBar/ProgressBar.vue
@@ -4,7 +4,6 @@ const { progressWidth } = useProgressBar();
 
 <template>
   <div class="relative w-full h-1 bg-gray-300 rounded-md overflow-hidden">
-    <!-- The progress bar filling -->
     <div
       data-test="progress"
       class="h-full bg-green-500 transition-all duration-300 ease-linear"
