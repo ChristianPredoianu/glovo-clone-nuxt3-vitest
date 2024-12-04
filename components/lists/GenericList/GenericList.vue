5ff8de19f81641c937ad2b539b34cb0077c9bca9
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-04 13:49:28 +0100

    work

--- components/lists/GenericList/GenericList.vue
+++ components/lists/GenericList/GenericList.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
 import type { GenericListProps } from '@/interfaces/interfaces.interface';
+import { capitalizeFirstLetter } from '@/helpers/capitalizeFirstLetter';
+
+interface Item {
+  [key: string]: any;
+}
 
 const props = defineProps<GenericListProps>();
+
+function getItemField(item: Item, field: string): string {
+  const nestedFields = field.split('.');
+
+  let value: any = item;
+
+  for (const nestedField of nestedFields) {
+    if (value == null) return '';
+    value = value[nestedField];
+  }
+
+  return value != null ? String(value) : '';
+}
 </script>
 
 <template>
   <ul class="flex flex-wrap gap-2 justify-around">
-    <GenericListItem :items="items" :field="props.field" />
+    <li
+      v-for="(item, key) in props.items"
+      :key="key"
+      class="bg-orange-200 text-lg font-semibold p-4 rounded-3xl cursor-pointer"
+    >
+      {{ capitalizeFirstLetter(getItemField(item, props.field || '')) }}
+    </li>
   </ul>
 </template>
