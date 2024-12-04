ded4128ce62182607a3a4b78bf7b95522b34b746
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-04 14:43:05 +0100

    fixed defineProps

--- components/modals/ProductModal/List/ProductModalList.vue
+++ components/modals/ProductModal/List/ProductModalList.vue
@@ -2,8 +2,6 @@
 import type { IIngredient } from '@/interfaces/interfaces.interface.ts';
 
 const props = defineProps({
-  foodId: { type: String, required: true },
-  text: { type: String, required: true },
   ingredients: {
     type: Array as () => IIngredient[],
   },
