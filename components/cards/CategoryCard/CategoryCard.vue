43a3fcabf954f6da04dd793aa3c61a0c5bc6421c
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-05 13:21:42 +0100

    changed to nuxt image

--- components/cards/CategoryCard/CategoryCard.vue
+++ components/cards/CategoryCard/CategoryCard.vue
@@ -23,10 +23,15 @@ async function navigateToPage(selectedCategory: ISelectedCategory) {
     @click="navigateToPage(props.productCategory)"
   >
     <div class="relative w-24 h-24 xl:w-32 xl:h-32 flex items-center justify-center">
-      <img
+      <NuxtImg
         :src="props.productCategory.img"
         :alt="props.productCategory.text"
         class="w-full shadow-lg shadow-orange-200 h-full object-cover transform transition-transform duration-500 ease-in-out rounded-[40%_60%_70%_30%_/_30%_50%_60%_40%] hover:rounded-[60%_40%_50%_50%_/_50%_30%_40%_60%] hover:scale-105"
+        format="webp"
+        loading="lazy"
+        fit="cover"
+        width="128"
+        height="128"
       />
     </div>
     <h3 class="text-gray-700 mt-3 text-center font-semibold">
