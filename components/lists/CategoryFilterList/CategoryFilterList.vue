28e14bfb21c857ca3e2802770b905712354b6861
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-09 18:25:05 +0100

    added folder

---
+++ components/lists/CategoryFilterList/CategoryFilterList.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type {
+  IFakeStoreCategories,
+  ICuisineType,
+} from '@/interfaces/interfaces.interface';
+
+const emits = defineEmits(['emitSelected']);
+
+const { filters } = useFilter();
+
+const { setActive, isActive } = useIsActive();
+const { getCategoryName } = useFilter();
+
+function handleClick(selectedFilter: IFakeStoreCategories | ICuisineType) {
+  const categoryName = getCategoryName(selectedFilter);
+  emits('emitSelected', selectedFilter);
+  setActive(categoryName);
+}
+</script>
+
+<template>
+  <ul class="flex flex-col gap-y-2">
+    <li
+      v-for="category in filters"
+      :key="category.id"
+      class="flex gap-x-2 items-center pb-2 border-solid border-b-1 border-gray-100 cursor-pointer"
+      @click="handleClick(category)"
+    >
+      <font-awesome-icon
+        data-test="icon"
+        :icon="['fas', category?.icon]"
+        class="fa-fw bg-orange-200 p-2 rounded-full text-gray-600"
+        :class="{ 'bg-orange-300': isActive(getCategoryName(category)) }"
+      />
+      <p :class="{ 'font-semibold': isActive(getCategoryName(category)) }">
+        {{ getCategoryName(category) }}
+      </p>
+    </li>
+  </ul>
+</template>
