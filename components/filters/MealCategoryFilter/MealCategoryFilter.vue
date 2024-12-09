7f296aaab980fac81cb62bab5ceecb53a6178fa4
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-09 18:12:09 +0100

    extracted list

---
+++ components/filters/MealCategoryFilter/MealCategoryFilter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type {
+  IFakeStoreCategories,
+  ICuisineType,
+} from '@/interfaces/interfaces.interface';
+
+const emits = defineEmits(['emitSelected']);
+
+function handleEmitSelected(selectedFilter: IFakeStoreCategories | ICuisineType) {
+  emits('emitSelected', selectedFilter);
+}
+</script>
+
+<template>
+  <MealCategoryFilterList @emitSelected="handleEmitSelected" />
+</template>
