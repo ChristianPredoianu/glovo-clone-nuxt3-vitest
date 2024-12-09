581c014003b134794169ea2bf43acd57a156a9d5
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-09 18:24:55 +0100

    added folder

---
+++ components/filters/CategoryFilter/CategoryFilter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type {
+  IFakeStoreCategories,
+  ICuisineType,
+} from '@/interfaces/interfaces.interface';
+
+const emits = defineEmits(['emitSelected']);
+
+function handleEmitSelected(selectedFilter: IFakeStoreCategories | ICuisineType) {
+  emits('emitSelected', selectedFilter);
+}
+</script>
+
+<template>
+  <CategoryFilterList @emitSelected="handleEmitSelected" />
+</template>
