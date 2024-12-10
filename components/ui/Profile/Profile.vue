9e0887acf0ec0545052c9e6df26c1e08d3850d1a
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-10 15:03:38 +0100

    work

--- components/ui/Profile/Profile.vue
+++ components/ui/Profile/Profile.vue
@@ -4,13 +4,9 @@ const { isLoaded } = useIsLoaded();
 </script>
 
 <template>
-  <div class="inline-flex flex-col items-center justify-end md:flex-row gap-x-10 w-auto">
-    <div class="flex flex-col gap-y-1">
-      <p v-if="user?.email" data-test="email-element" class="font-semibold text-sm">
-        {{ user.email }}
-      </p>
-      <p class="text-xs">Admin</p>
-    </div>
+  <div
+    class="inline-flex py-4 flex-col items-center justify-end md:flex-row gap-x-10 w-auto"
+  >
     <div
       class="flex items-center justify-center bg-gray-200 rounded-full w-12 h-12 lg:w-16 lg:h-16"
     >
@@ -20,6 +16,12 @@ const { isLoaded } = useIsLoaded();
         :icon="['fa', 'fa-user']"
         class="text-xl lg:text-2xl"
       />
+    </div>
+    <div class="flex flex-col gap-y-1 mt-4">
+      <p v-if="user?.email" data-test="email-element" class="font-semibold text-sm">
+        {{ user.email }}
+      </p>
+      <p class="text-xs">Admin</p>
     </div>
   </div>
 </template>
