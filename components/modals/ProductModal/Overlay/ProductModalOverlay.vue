9f33482bc756d3de6600aa0bf924463a6ded913d
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-10 13:26:36 +0100

    work on price

--- components/modals/ProductModal/Overlay/ProductModalOverlay.vue
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.vue
@@ -7,7 +7,7 @@ const props = defineProps<{ productModalProps: ModalProps | null }>();
 const emits = defineEmits(['closeModal']);
 
 const { addToCart } = useCart();
-const { progressWidth, isBarActive, startProgressBar } = useProgressBar();
+const { progressWidth, isBarActive } = useProgressBar();
 
 const product = computed(() => {
   if (!props.productModalProps) return null;
