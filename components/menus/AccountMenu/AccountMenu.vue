8e410a422b304c6d7e94f0fb3517c6fd427b3576
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-11 13:56:19 +0100

    work

--- components/menus/AccountMenu/AccountMenu.vue
+++ components/menus/AccountMenu/AccountMenu.vue
@@ -14,9 +14,9 @@ function handleClick(menuItem: IMenuItem) {
 </script>
 
 <template>
-  <ul class="sm:w-3/4 lg:w-2/5 flex flex-col gap-y-1">
+  <ul class="w-full lg:w-2/5 flex flex-col gap-y-1">
     <li
-      class="bg-gray-300 py-2"
+      class="bg-gray-300 py-2 cursor-pointer"
       v-for="menuItem in menuData"
       :key="menuItem.id"
       @click="handleClick(menuItem)"
@@ -26,14 +26,14 @@ function handleClick(menuItem: IMenuItem) {
           data-test="fa-user"
           v-if="isLoaded"
           :icon="menuItem.icon"
-          class="text-xl p-2 lg:text-2xl border-r-2 flex-shrink-0 border-gray-500 w-1/6"
+          class="text-xs p-2 border-r-2 flex-shrink-0 border-gray-500 w-1/6"
         />
         <span class="pl-2 flex-grow">{{ menuItem.menuText }}</span>
         <font-awesome-icon
           data-test="fa-chevron-right"
           v-if="isLoaded"
           :icon="['fa', 'fa-chevron-right']"
-          class="text-xl lg:text-2xl flex-shrink-0 pr-2 w-1/6"
+          class="text-xs flex-shrink-0 pr-2 w-1/6"
         />
       </div>
     </li>
