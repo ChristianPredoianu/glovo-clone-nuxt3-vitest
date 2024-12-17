510461a6c7c5f06722dc39afabce285e23eeec40
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-17 16:15:50 +0100

    work

--- components/buttons/FormSubmitBtn/FormSubmitBtn.vue
+++ components/buttons/FormSubmitBtn/FormSubmitBtn.vue
@@ -1,7 +1,8 @@
 <template>
   <button
     type="submit"
-    class="w-full bg-green-600 text-gray-100 py-2 px-4 rounded-md hover:bg-green-700 transition-colors duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-opacity-50"
+    aria-label="Submit Form"
+    class="w-full bg-green-600 text-gray-100 py-2 px-4 rounded-md hover:bg-green-700 transition-opacity duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-opacity-50"
   >
     <slot></slot>
   </button>
