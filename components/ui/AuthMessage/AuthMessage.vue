22e2642bd32f52e2c2bac43aaefd9119444ca7fe
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-17 15:59:41 +0100

    added component

---
+++ components/ui/AuthMessage/AuthMessage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  authErrorMessage: String,
+  successMessage: String,
+});
+</script>
+
+<template>
+  <p
+    class="my-1 text-sm font-semibold"
+    :class="authErrorMessage ? 'text-red-500' : 'text-green-500'"
+  >
+    {{ authErrorMessage || successMessage }}
+  </p>
+</template>
