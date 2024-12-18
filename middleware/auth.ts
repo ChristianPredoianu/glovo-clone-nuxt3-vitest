ad6176f0169c551fa2752c349465348bf88dc192
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-18 13:54:02 +0100

    added funtion to update profile

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const { user, isAuthReady } = useAuth(to.query.redirect as string);
+  // Wait until Firebase authentication is ready
+  if (!isAuthReady.value) {
+    console.log('Auth not ready yet...');
+    return;
+  }
 
-  if (!isAuthReady.value) return; // Don't run the middleware until Firebase is ready
+  console.log('User:', user.value); // Check user value
 
+  // If user is not authenticated, redirect to home page
   if (!user.value) {
+    console.log('Redirecting to home...');
     return navigateTo({
       path: '/',
       query: { redirect: to.fullPath },
