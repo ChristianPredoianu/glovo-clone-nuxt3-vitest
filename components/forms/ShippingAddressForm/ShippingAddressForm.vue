b8368d2a3eabfe849b36a7db4ee41eecf46a6c4f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-19 14:46:43 +0100

    work

---
+++ components/forms/ShippingAddressForm/ShippingAddressForm.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { handleKeyDown } from '@/helpers/helpers';
+import useShippingFormValidation from '@/composables/form-validations/useShippingFormValidation';
+
+const address = reactive({
+  streetAndHouseNumber: '',
+  zipCode: '',
+  city: '',
+  country: '',
+});
+
+const { user, isAuthReady, successMessage, authErrorMessage } = useAuth();
+const {
+  validateStreetAndHouseNumber,
+  validateEuropeanZipCode,
+  validateCityOrCountry,
+  validateAllFields,
+  errors,
+} = useShippingFormValidation();
+
+async function handleUpdateShipping(e: Event) {
+  e.preventDefault();
+
+  if (validateAllFields(address)) {
+  }
+}
+
+/* watch(
+  () => user.value,
+  (newUser) => {
+    if (newUser && isAuthReady) {
+      userName.value = newUser.displayName || '';
+      userEmail.value = newUser.email || '';
+    }
+  },
+  { immediate: true }
+); */
+
+function validateForm(): boolean {
+  /*  validateUserName(userName.value);
+  validateEmail(userEmail.value);
+
+  return !userNameError.value && !emailError.value; */
+}
+
+function onKeyDown(e: KeyboardEvent) {
+  handleKeyDown(e, handleUpdateShipping);
+}
+</script>
+
+<template>
+  <h1 class="text-2xl font-semibold text-gray-800 my-5">Shipping info</h1>
+  <form
+    @submit.prevent="handleUpdateShipping"
+    @keydown="onKeyDown"
+    class="flex flex-col gap-2"
+  >
+    <TextInput
+      label="Street and house number"
+      name="street"
+      v-model="address.streetAndHouseNumber"
+      placeholder="Street and house number"
+      autocomplete="address-line1"
+      @blur="validateStreetAndHouseNumber(address.streetAndHouseNumber)"
+    />
+    <p v-if="errors.streetAndHouseNumber" class="text-red-600 text-xs mt-1">
+      {{ errors.streetAndHouseNumber }}
+    </p>
+
+    <TextInput
+      label="Zip code"
+      name="zip code"
+      v-model="address.zipCode"
+      placeholder="Zip code"
+      autocomplete="postal-code"
+      @blur="validateEuropeanZipCode(address.zipCode)"
+    />
+    <p v-if="errors.zipCode" class="text-red-600 text-xs mt-1">
+      {{ errors.zipCode }}
+    </p>
+
+    <TextInput
+      label="City"
+      name="city"
+      v-model="address.city"
+      placeholder="City"
+      autocomplete="address-level2"
+      @blur="validateCityOrCountry(address.city, 'city')"
+    />
+    <p v-if="errors.city" class="text-red-600 text-xs mt-1">
+      {{ errors.city }}
+    </p>
+
+    <TextInput
+      label="Country"
+      name="country"
+      v-model="address.country"
+      placeholder="Country"
+      autocomplete="country"
+      @blur="validateCityOrCountry(address.country, 'country')"
+    />
+    <p v-if="errors.country" class="text-red-600 text-xs mt-1">
+      {{ errors.country }}
+    </p>
+
+    <FormSubmitBtn class="mt-10">Update</FormSubmitBtn>
+  </form>
+</template>
