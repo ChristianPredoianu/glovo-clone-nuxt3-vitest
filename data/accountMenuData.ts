7a294b1a2d2756c91e1035ce3346212c2e7a78e0
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-19 14:46:59 +0100

    removed menuItem

--- data/accountMenuData.ts
+++ data/accountMenuData.ts
@@ -1,25 +1,20 @@
 export const menuData = [
   { id: 1, icon: ['fa', 'fa-user'], menuText: 'My Account', component: 'MyAccountForm' },
+
   {
     id: 2,
-    icon: ['fa', 'fa-credit-card'],
-    menuText: 'Billing Address',
-    component: 'BillingAddressForm',
-  },
-  {
-    id: 3,
     icon: ['fa', 'fa-truck-fast'],
     menuText: 'Shipping Address',
     component: 'ShippingAddressForm',
   },
   {
-    id: 4,
+    id: 3,
     icon: ['fa', 'fa-cart-shopping'],
     menuText: 'My orders',
     component: 'MyOrders',
   },
   {
-    id: 5,
+    id: 4,
     icon: ['fa', 'fa-trash'],
     menuText: 'Delete Account',
     component: 'DeleteAccount',
