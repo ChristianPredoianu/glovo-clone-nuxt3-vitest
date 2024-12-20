8a05eb8c0c3b18298c6b6778472835a2e7cd2afe
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-20 12:59:06 +0100

    fixed types imports

--- components/ui/Dropdown/SearchLocationDropdown/SearchLocationDropdown.vue
+++ components/ui/Dropdown/SearchLocationDropdown/SearchLocationDropdown.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { IDropdownOptions } from '@/interfaces/interfaces.interface';
+import type { IDropdownOptions } from '@/types/ui';
 
 const props = defineProps({
   options: {
