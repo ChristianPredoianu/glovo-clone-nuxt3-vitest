2c6a59039c34a876fea3fe26ba7dae0da217d85d
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-20 12:58:47 +0100

    fixed types imports

--- composables/form-validations/useShippingFormValidation.ts
+++ composables/form-validations/useShippingFormValidation.ts
@@ -1,17 +1,10 @@
-import { ref } from 'vue';
 import { capitalizeFirstLetter } from '@/helpers/helpers';
+import type { IShippingAddress } from '@/types/locations';
 
 type AddressField = 'streetAndHouseNumber' | 'zipCode' | 'city' | 'country';
 
-interface Address {
-  streetAndHouseNumber: string | undefined;
-  zipCode: string | undefined;
-  city: string | undefined;
-  country: string | undefined;
-}
-
 export default function useShippingFormValidation() {
-  const errors = ref({
+  const errors = reactive({
     streetAndHouseNumber: '',
     zipCode: '',
     city: '',
@@ -19,19 +12,17 @@ export default function useShippingFormValidation() {
   });
 
   function clearErrors() {
-    errors.value = {
-      streetAndHouseNumber: '',
-      zipCode: '',
-      city: '',
-      country: '',
-    };
+    errors.streetAndHouseNumber = '';
+    errors.zipCode = '';
+    errors.city = '';
+    errors.country = '';
   }
 
   function validateStreetAndHouseNumber(value: string | undefined) {
-    errors.value.streetAndHouseNumber = '';
+    errors.streetAndHouseNumber = '';
 
     if (!value || value.trim() === '') {
-      errors.value.streetAndHouseNumber = 'Street and house number is required.';
+      errors.streetAndHouseNumber = 'Street and house number is required.';
       return false;
     }
 
@@ -41,8 +32,7 @@ export default function useShippingFormValidation() {
     const regex = /^[\p{L}\s]+ \d+$/u;
 
     if (!regex.test(trimmedValue)) {
-      errors.value.streetAndHouseNumber =
-        'Invalid format. Please use the format "Street 2".';
+      errors.streetAndHouseNumber = 'Invalid format. Please use the format "Street 2".';
       return false;
     }
 
@@ -50,10 +40,10 @@ export default function useShippingFormValidation() {
   }
 
   function validateEuropeanZipCode(value: string | undefined) {
-    errors.value.zipCode = '';
+    errors.zipCode = '';
 
     if (!value || value.trim() === '') {
-      errors.value.zipCode = 'Zip code is required.';
+      errors.zipCode = 'Zip code is required.';
       return false;
     }
 
@@ -62,7 +52,7 @@ export default function useShippingFormValidation() {
     const europeanZipCodeRegex = /^[A-Z0-9]{3,4}(\s?[A-Z0-9]{2,4})?$/i;
 
     if (!europeanZipCodeRegex.test(trimmedValue)) {
-      errors.value.zipCode = 'Invalid zip code. Please enter a valid European zip code.';
+      errors.zipCode = 'Invalid zip code. Please enter a valid European zip code.';
       return false;
     }
 
@@ -70,10 +60,10 @@ export default function useShippingFormValidation() {
   }
 
   function validateCityOrCountry(value: string | undefined, fieldName: AddressField) {
-    errors.value[fieldName] = '';
+    errors[fieldName] = '';
 
     if (!value || value.trim() === '') {
-      errors.value[fieldName] = `${capitalizeFirstLetter(fieldName)} is required.`;
+      errors[fieldName] = `${capitalizeFirstLetter(fieldName)} is required.`;
       return false;
     }
 
@@ -82,7 +72,7 @@ export default function useShippingFormValidation() {
     const regex = /^[a-zA-ZÀ-ÿ\s'-]+$/;
 
     if (!regex.test(trimmedValue)) {
-      errors.value[
+      errors[
         fieldName
       ] = `${fieldName} is invalid. Only letters, spaces, apostrophes, hyphens, and accents are allowed.`;
       return false;
@@ -91,7 +81,7 @@ export default function useShippingFormValidation() {
     return true;
   }
 
-  function validateAllFields(address: Address) {
+  function validateAllFields(address: IShippingAddress) {
     clearErrors();
 
     const isStreetValid = validateStreetAndHouseNumber(address.streetAndHouseNumber);
