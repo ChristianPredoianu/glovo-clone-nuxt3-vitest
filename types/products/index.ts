f79000cf82bd1001fd11fc4692b719cddbbfadbe
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-20 12:30:28 +0100

    added exports

--- types/products/index.ts
+++ types/products/index.ts
@@ -1,1 +1,7 @@
+export * from './ICartProducts';
+export * from './IFakeStoreCategories';
+export * from './IItem';
+export * from './IProduct';
+export * from './IProductCategories';
 export * from './IProductModalProps';
+export * from './ISelectedCategory';
