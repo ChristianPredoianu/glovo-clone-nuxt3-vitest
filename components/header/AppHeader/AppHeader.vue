73f567b1b73fdf6dcc4c28a1fe81458c0d5710ce
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-24 11:29:26 +0100

    work

--- components/header/AppHeader/AppHeader.vue
+++ components/header/AppHeader/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Modal from '@/components/modals/Modal/Modal.vue';
 
-const { isNavOpen, closeNav } = useNav();
+const { isNavOpen, closeNav, openNav } = useNav();
 const { closeBackdrop } = useBackdrop();
 const { screenWidth } = useScreenWidth();
 const { user, signUserOut } = useAuth();
@@ -42,7 +42,7 @@ function signOut() {
       <Hamburger />
       <div
         id="nav-list-div"
-        :class="isNavOpen ? ['translate-x-[0rem]'] : ['-translate-x-[50rem]']"
+        :class="isNavOpen ? 'translate-x-0' : '-translate-x-full'"
         class="h-full bg-white sm:bg-transparent fixed sm:relative z-40 top-0 left-0 w-10/12 sm:w-auto min-h-screen sm:min-h-full flex flex-grow flex-col items-center justify-center sm:justify-start sm:flex-row rounded-r-2xl sm:rounded-r-none transition ease-in-out sm:py-0 sm:translate-x-0 sm:ml-4"
       >
         <NavItems />
