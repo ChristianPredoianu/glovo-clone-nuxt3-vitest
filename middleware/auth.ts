972215c2c5a02ff9588d940d0ccd1a78e5f48296
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-24 11:29:07 +0100

    final

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,19 +1,19 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const { user, isAuthReady } = useAuth(to.query.redirect as string);
-  // Wait until Firebase authentication is ready
-  if (!isAuthReady.value) {
-    console.log('Auth not ready yet...');
-    return;
-  }
+  try {
+    const { user, isAuthReady } = useAuth(to.query.redirect as string);
 
-  console.log('User:', user.value); // Check user value
+    await isAuthReady.value;
 
-  // If user is not authenticated, redirect to home page
-  if (!user.value) {
-    console.log('Redirecting to home...');
-    return navigateTo({
-      path: '/',
-      query: { redirect: to.fullPath },
-    });
+    console.log('User:', user.value);
+
+    if (!user.value) {
+      return navigateTo({
+        path: '/',
+        query: { redirect: to.fullPath },
+      });
+    }
+  } catch (error) {
+    console.error('Error in auth middleware:', error);
+    return navigateTo({ path: '/', query: { redirect: to.fullPath } });
   }
 });
