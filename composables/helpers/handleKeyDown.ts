4ef7c1247cf188b4df8b6a6a4f95ad58724f3094
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-30 12:26:16 +0100

    changed name

--- composables/helpers/handleKeyDown.ts
+++
@@ -1,9 +0,0 @@
-interface ICallbackFunction {
-  (e: Event): void;
-}
-
-export function handleKeyDown(e: KeyboardEvent, callback: ICallbackFunction) {
-  if (e.key === 'Enter') {
-    callback(e);
-  }
-}
