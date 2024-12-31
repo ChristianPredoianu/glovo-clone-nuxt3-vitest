07d308fe61731a9cd868fe74a61f391c36317dde
Christian Predoianu <christianpredoianu@yahoo.com>
2024-12-31 16:49:54 +0100

    commented out

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,19 +1,27 @@
+const protectedRoutes = ['/dashboard', '/profile', '/settings'];
+
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  /*  const { user, isAuthReady } = useAuth(to.query.redirect as string);
+  console.log(to.query.redirect as string);
   try {
-    const { user, isAuthReady } = useAuth(to.query.redirect as string);
+    console.log(to.query.redirect as string);
 
+    console.log('isAuthReady:', isAuthReady.value);
     await isAuthReady.value;
+    console.log('User after isAuthReady:', user.value);
 
-    console.log('User:', user.value);
-
-    if (!user.value) {
-      return navigateTo({
-        path: '/',
-        query: { redirect: to.fullPath },
-      });
+    // Check if the route is in the protected list
+    if (protectedRoutes.includes(to.path)) {
+      if (!user.value) {
+        console.log('User is not signed in, redirecting...');
+        return navigateTo({
+          path: '/',
+          query: { redirect: to.fullPath }, // Redirect to login page if not authenticated
+        });
+      }
     }
   } catch (error) {
     console.error('Error in auth middleware:', error);
     return navigateTo({ path: '/', query: { redirect: to.fullPath } });
-  }
+  } */
 });
