eb87949a6989d69975b13653a1e3b9484a619984
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-04 17:07:52 +0100

    added

---
+++ composables/firebase/auth/store/authStore.ts
@@ -0,0 +1,16 @@
+import type { User, Auth } from 'firebase/auth';
+
+declare module '#app' {
+  interface NuxtApp {
+    $auth: Auth;
+  }
+}
+
+export const user = ref<User | null>(null);
+export const isAuthReady = ref(false);
+export const successMessage = ref<string | null>(null);
+export const authErrorMessage = ref<string | null>(null);
+export const userNameError = ref('');
+export const emailError = ref<string | null>(null);
+export const passwordError = ref<string | null>(null);
+export const repeatedPasswordError = ref<string | null>(null);
