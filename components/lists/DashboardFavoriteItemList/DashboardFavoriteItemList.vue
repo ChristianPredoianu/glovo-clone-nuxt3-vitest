053c93b590ff0741437ccdbec38b1efabaf957f8
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-07 16:38:38 +0100

    work

--- components/lists/DashboardFavoriteItemList/DashboardFavoriteItemList.vue
+++ components/lists/DashboardFavoriteItemList/DashboardFavoriteItemList.vue
@@ -9,27 +9,42 @@ const props = defineProps({
     required: true,
   },
 });
+
+const { addToCart } = useCart();
 </script>
 
 <template>
-  <ul class="mt-4 space-y-4">
+  <ul class="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     <li
       v-for="item in displayedItems"
       :key="item.id"
-      class="flex items-center p-4 border-2 border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 ease-in-out"
+      class="flex flex-col items-center p-4 border-2 border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 ease-in-out"
     >
       <img
         src="../../../public/glovo.jpg"
         alt="Item Image"
-        class="w-32 h-32 object-cover rounded-lg mr-4"
+        class="w-40 h-40 object-cover rounded-lg mb-4 sm:w-48 sm:h-48 md:w-56 md:h-56"
       />
-      <div class="flex flex-col gap-y-1">
+
+      <div class="flex flex-col items-center gap-y-2 text-center sm:text-left">
         <p class="text-lg font-semibold text-gray-800">
           {{ capitalizeFirstLetter(item.category) }}
         </p>
-        <p class="text-sm text-gray-600">{{ replaceRecipeText(item.label) }}</p>
+        <p class="text-sm text-gray-600">
+          {{ replaceRecipeText(item.label) }}
+        </p>
       </div>
-      <p>{{ item }}</p>
+      <div
+        class="flex flex-col items-center sm:items-end gap-y-4 mt-4 sm:mt-0 sm:ml-auto"
+      >
+        <p class="text-2xl font-bold text-green-700">{{ item.price }}€</p>
+        <CtaBtn
+          class="px-6 py-2 mt-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition-all duration-300"
+          @click="addToCart(item)"
+        >
+          Add to Cart
+        </CtaBtn>
+      </div>
     </li>
   </ul>
 </template>
