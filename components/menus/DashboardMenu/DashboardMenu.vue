aa0dc14d1e690db43067462d3c31e186fc4953a4
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-07 16:35:55 +0100

    added component

---
+++ components/menus/DashboardMenu/DashboardMenu.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const emit = defineEmits(['changeComponent']);
+
+function handleClick(menuItem: string) {
+  const component = menuItem;
+
+  emit('changeComponent', component);
+}
+</script>
+
+<template>
+  <ul class="flex">
+    <li class="cursor-pointer" @click="handleClick('ItemFavoritesListSection')">
+      Your favorites
+    </li>
+    <li class="cursor-pointer" @click="handleClick('OrdersList')">Your orders</li>
+  </ul>
+</template>
