6a7d67e0e9663edabe8307086c8fe707cba3bf81
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-07 16:37:54 +0100

    work

--- components/modals/ProductModal/Overlay/ProductModalOverlay.vue
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { replaceRecipeText } from '@/composables/helpers/replaceRecipeText';
+
 import type { IMealModalProps } from '@/types/meals';
 import type { ModalProps } from '@/types/ui';
 
