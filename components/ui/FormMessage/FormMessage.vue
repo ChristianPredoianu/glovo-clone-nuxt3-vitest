e372366cc6e3da1314f126bc456c18b6ce8ac9a9
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-09 14:46:31 +0100

    changed name

---
+++ components/ui/FormMessage/FormMessage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  errorMessage: String,
+  successMessage: String,
+});
+</script>
+
+<template>
+  <p
+    class="my-1 text-sm font-semibold"
+    :class="errorMessage ? 'text-red-500' : 'text-green-500'"
+  >
+    {{ errorMessage || successMessage }}
+  </p>
+</template>
