cfadaedec25f24999ca408fee4fff33927876b63
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-13 11:47:57 +0100

    work

--- components/modals/AddressModal/Overlay/AddressModalOverlay.vue
+++ components/modals/AddressModal/Overlay/AddressModalOverlay.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { IShippingAddress } from '@/types/locations';
+
+const { user } = useAuth();
+const { $database } = useNuxtApp();
+const { writeAddressInfo } = useFirebaseAddressActions();
+
+function handleUpdateShipping(address: IShippingAddress){
+  writeAddressInfo(user.value!.uid, $database, address);
+}
+</script>
 
 <template>
-  <ShippingAddressForm />
+  <ShippingAddressForm @submitForm ="handleUpdateShipping"/>
+  <FormSubmitBtn class="mt-2">Place order</FormSubmitBtn>
 </template>
