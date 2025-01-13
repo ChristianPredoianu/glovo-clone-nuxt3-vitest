d91d6bb8a7ef23d3e1e06841bda4cba5f8494b44
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-13 14:04:29 +0100

    added section

---
+++ components/sections/OrdersListSection/OrdersListSection.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const { fetchOrders, fetchedOrders, isLoading } = useFirebaseOrderActions();
+
+const { isAuthReady } = useAuth();
+
+onMounted(() => {
+  if (isAuthReady.value) {
+    fetchOrders();
+  } else {
+    const stopWatching = watch(
+      () => isAuthReady.value,
+      (ready) => {
+        if (ready) {
+          fetchOrders();
+          stopWatching();
+        }
+      },
+      { immediate: true }
+    );
+  }
+});
+
+const { currentPage, itemsPerPage, totalItems, displayedItems, handlePageChange } =
+  usePagination(fetchedOrders, 5);
+</script>
+
+<template>
+  <LoadingSpinner v-if="isLoading" />
+
+  <section class="p-4 mt-4" v-if="fetchedOrders.length > 0">
+    <div class="flex flex-col items-center md:flex-row md:justify-between mt-4">
+      <Pagination
+        v-if="displayedItems.length > 0"
+        :currentPage="currentPage"
+        :totalItems="totalItems"
+        :itemsPerPage="itemsPerPage"
+        @pageChanged="handlePageChange"
+      />
+    </div>
+    <OrdersList :orderedItems="displayedItems" />
+    <h2 v-if="displayedItems.length === 0" class="text-xl font-semibold text-center">
+      You don't have any favorites in this category
+    </h2>
+  </section>
+</template>
