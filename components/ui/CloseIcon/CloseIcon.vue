a6f3f1f8de16a683aaff44527d284284dc247194
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-13 11:48:31 +0100

    removed mb

--- components/ui/CloseIcon/CloseIcon.vue
+++ components/ui/CloseIcon/CloseIcon.vue
@@ -6,7 +6,7 @@ const emits = defineEmits(['close']);
   <font-awesome-icon
     :icon="['fas', 'close']"
     data-test="fa-close"
-    class="text-2xl text-right mb-8 font-semibold cursor-pointer"
+    class="text-2xl text-right font-semibold cursor-pointer"
     @click="emits('close')"
   />
 </template>
