88a45dd8b3e7184e441cb2bf0ea35af2e312dcd5
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-13 14:05:54 +0100

    work

--- components/modals/AddressModal/Overlay/AddressModalOverlay.vue
+++ components/modals/AddressModal/Overlay/AddressModalOverlay.vue
@@ -4,13 +4,26 @@ import type { IShippingAddress } from '@/types/locations';
 const { user } = useAuth();
 const { $database } = useNuxtApp();
 const { writeAddressInfo } = useFirebaseAddressActions();
+const { writeOrderDetails } = useFirebaseOrderActions();
+const { numberOfCartProducts, cartProducts, updatedTotalPrice } = useCart();
 
-function handleUpdateShipping(address: IShippingAddress){
+function handleUpdateShipping(address: IShippingAddress) {
   writeAddressInfo(user.value!.uid, $database, address);
+}
+
+function placeOrder() {
+  const order = {
+    products: cartProducts.value,
+    totalPrice: updatedTotalPrice.value,
+    numberOfProducts: numberOfCartProducts.value,
+  };
+
+  writeOrderDetails(order);
+  console.log(order);
 }
 </script>
 
 <template>
-  <ShippingAddressForm @submitForm ="handleUpdateShipping"/>
-  <FormSubmitBtn class="mt-2">Place order</FormSubmitBtn>
+  <ShippingAddressForm @submitForm="handleUpdateShipping" />
+  <FormSubmitBtn class="mt-2" @click="placeOrder">Place order</FormSubmitBtn>
 </template>
