1e03984c35f8063d21e1f03775a453c0477b6eda
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-13 14:06:13 +0100

    work

--- components/lists/OrdersList/OrdersList.vue
+++ components/lists/OrdersList/OrdersList.vue
@@ -1,5 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { IOrder } from '@/types/cart/IOrder';
+
+const props = defineProps({
+  orderedItems: {
+    type: Array as PropType<IOrder[]>,
+    required: true,
+  },
+});
+
+const expanded = ref<string | number | null>(null);
+
+function toggleExpand(id: string | number) {
+  expanded.value = expanded.value === id ? null : id;
+}
+</script>
 
 <template>
-  <h1>Orders list</h1>
+  <ul>
+    <li
+      v-for="order in orderedItems"
+      :key="order.id"
+      :class="{ expanded: expanded === order.id }"
+      @click="toggleExpand(order.id!)"
+    >
+      {{ order.id }}
+      <div v-if="expanded === order.id" class="expanded-content">
+        <!-- Content to display when the li is expanded -->
+        More details about the order:
+      </div>
+    </li>
+  </ul>
 </template>
