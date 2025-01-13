6752348deadd197de5fc3a68b6aa6faf3d03e0a5
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-13 14:06:01 +0100

    work

--- components/menus/DashboardMenu/DashboardMenu.vue
+++ components/menus/DashboardMenu/DashboardMenu.vue
@@ -13,6 +13,6 @@ function handleClick(menuItem: string) {
     <li class="cursor-pointer" @click="handleClick('ItemFavoritesListSection')">
       Your favorites
     </li>
-    <li class="cursor-pointer" @click="handleClick('OrdersList')">Your orders</li>
+    <li class="cursor-pointer" @click="handleClick('OrdersListSection')">Your orders</li>
   </ul>
 </template>
