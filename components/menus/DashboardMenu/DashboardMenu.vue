c5ebf48af9202414f7a8cf69cc14d62d41ce4e50
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-21 16:22:00 +0100

    fixed the menu with active classes

--- components/menus/DashboardMenu/DashboardMenu.vue
+++ components/menus/DashboardMenu/DashboardMenu.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
+import { menuItems } from '@/data/dashboardMenuData';
+
 const emit = defineEmits(['changeComponent']);
 
-function handleClick(menuItem: string) {
-  const component = menuItem;
+const activeSection = ref('');
 
-  emit('changeComponent', component);
+onMounted(() => {
+  const ItemFavoritesSection = menuItems[0].section;
+  activeSection.value = ItemFavoritesSection;
+});
+
+function handleClick(section: string) {
+  activeSection.value = section;
+  emit('changeComponent', section);
 }
 </script>
 
 <template>
-  <ul class="flex">
-    <li class="cursor-pointer" @click="handleClick('ItemFavoritesListSection')">
-      Your favorites
+  <ul class="flex items-center gap-4 mt-4 text-gray-100 font-medium">
+    <li
+      v-for="item in menuItems"
+      :key="item.id"
+      :class="[
+        'py-2 px-4 cursor-pointer transition-all duration-200 rounded',
+        activeSection === item.section
+          ? 'bg-green-600 text-gray-100 hover:bg-green-700'
+          : 'border border-green-600 text-gray-900 hover:bg-green-700 hover:text-gray-100',
+      ]"
+      @click="handleClick(item.section)"
+    >
+      {{ item.name }}
     </li>
-    <li class="cursor-pointer" @click="handleClick('OrdersListSection')">Your orders</li>
   </ul>
 </template>
