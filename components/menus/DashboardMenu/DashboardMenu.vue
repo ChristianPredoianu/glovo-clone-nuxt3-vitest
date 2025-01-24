b7a76b430184e7c2afc964714d1ec01cfdabb8f8
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-24 11:55:14 +0100

    change vfor to menuItem

--- components/menus/DashboardMenu/DashboardMenu.vue
+++ components/menus/DashboardMenu/DashboardMenu.vue
@@ -19,17 +19,17 @@ function handleClick(section: string) {
 <template>
   <ul class="flex items-center gap-4 mt-4 text-gray-100 font-medium">
     <li
-      v-for="item in menuItems"
-      :key="item.id"
+      v-for="menuItem in menuItems"
+      :key="menuItem.id"
       :class="[
         'py-2 px-4 cursor-pointer transition-all duration-200 rounded',
-        activeSection === item.section
+        activeSection === menuItem.section
           ? 'bg-green-600 text-gray-100 hover:bg-green-700'
           : 'border border-green-600 text-gray-900 hover:bg-green-700 hover:text-gray-100',
       ]"
-      @click="handleClick(item.section)"
+      @click="handleClick(menuItem.section)"
     >
-      {{ item.name }}
+      {{ menuItem.name }}
     </li>
   </ul>
 </template>
