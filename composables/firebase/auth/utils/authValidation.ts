9fd0dd959826a086b41535158034f3169462fa6a
Christian Predoianu <christianpredoianu@yahoo.com>
2025-01-29 14:49:54 +0100

    refactored

--- composables/firebase/auth/utils/authValidation.ts
+++ composables/firebase/auth/utils/authValidation.ts
@@ -1,7 +1,5 @@
 export function validateCredentials(email: string, password: string) {
-  if (!email || !password) {
-    throw new Error('Email and password are required.');
-  }
+  if (!email || !password) throw new Error('Email and password are required.');
 }
 
 export function validateRepeatedPassword(password: string, repeatedPassword: string) {
