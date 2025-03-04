7eeacc307049bd3d3cc155ad4cfb384342530911
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-04 11:51:50 +0100

    added disabled

--- components/buttons/FormSubmitBtn/FormSubmitBtn.vue
+++ components/buttons/FormSubmitBtn/FormSubmitBtn.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  disabled: Boolean,
+});
+</script>
 <template>
   <button
     type="submit"
     aria-label="Submit Form"
-    class="w-full bg-green-600 text-gray-100 py-2 px-4 rounded-md hover:bg-green-700 transition-opacity duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-opacity-50"
+    :disabled="disabled"
+    class="w-full bg-green-600 text-gray-100 py-2 px-4 rounded-md hover:bg-green-700 transition-opacity duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
   >
     <slot></slot>
   </button>
