e0b4a9204ac9c94503ba8253da2722c1347c3278
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-05 12:34:02 +0100

    work

--- components/buttons/FormSubmitBtn/FormSubmitBtn.vue
+++ components/buttons/FormSubmitBtn/FormSubmitBtn.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 const props = defineProps({
   disabled: Boolean,
+  bgColor: {
+    type: String,
+    default: 'bg-green-600',
+  },
 });
 </script>
 <template>
@@ -8,7 +12,8 @@ const props = defineProps({
     type="submit"
     aria-label="Submit Form"
     :disabled="disabled"
-    class="w-full bg-green-600 text-gray-100 py-2 px-4 rounded-md hover:bg-green-700 transition-opacity duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+    class="text-gray-100 py-2 px-4 rounded-md hover:bg-green-700 transition-opacity duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+    :class="bgColor"
   >
     <slot></slot>
   </button>
