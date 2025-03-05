7c910801c822c06ed9d6fca3f09da15535576d98
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-05 10:33:53 +0100

    added links

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,9 +1,9 @@
 import type { INavLinks } from '@/types/menu';
 
 export const navLinks: INavLinks[] = [
-  { title: 'Home', link: '', icon: ['fas', 'house'] },
-  { title: 'Dashboard', link: '', icon: ['fas', 'user-tie'] },
-  { title: 'My Account', link: '', icon: ['fas', 'face-smile'] },
+  { title: 'Home', link: '/', icon: ['fas', 'house'] },
+  { title: 'Dashboard', link: '/dashboard', icon: ['fas', 'user-tie'] },
+  { title: 'My Account', link: '/my-account', icon: ['fas', 'face-smile'] },
   { title: 'Cart', link: '', icon: ['fas', 'cart-shopping'] },
   { title: 'Sign in', link: '', icon: ['fas', 'right-to-bracket'] },
 ];
