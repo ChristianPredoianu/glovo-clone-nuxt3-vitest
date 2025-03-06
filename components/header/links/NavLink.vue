af1f00ad732c34c1ecdbae386315c50e64b573a8
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-06 09:45:39 +0100

    work

--- components/header/links/NavLink.vue
+++ components/header/links/NavLink.vue
@@ -10,6 +10,9 @@ const props = defineProps({
 const { screenWidth } = useScreenWidth();
 const { numberOfCartProducts } = useCart();
 const { user } = useAuth();
+const { openModal } = useModal();
+const { closeNav } = useNav();
+
 const isAuthenticated = ref(user);
 
 const shouldShowNavItems = computed(() => {
@@ -24,6 +27,15 @@ const shouldShowAuthNavItems = computed(() => {
     isAuthenticated.value || (props.title !== 'My Account' && props.title !== 'Dashboard')
   );
 });
+
+const isCartLink = computed(() => {
+  return props.title === 'Cart';
+});
+
+function handleCartClick() {
+  openModal('cart');
+  closeNav();
+}
 </script>
 
 <template>
@@ -34,7 +46,7 @@ const shouldShowAuthNavItems = computed(() => {
       'text-xs': screenWidth < 230,
     }"
   >
-    <NuxtLink :to="props.link" class="w-full">
+    <NuxtLink v-if="!isCartLink" :to="props.link" @click="closeNav" class="w-full">
       <div
         class="w-full flex items-center justify-between border-b-2 sm:mt-1 sm:border-0 py-2 cursor-pointer"
       >
@@ -44,6 +56,17 @@ const shouldShowAuthNavItems = computed(() => {
         <font-awesome-icon :icon="props.icon" v-if="screenWidth < 640" />
       </div>
     </NuxtLink>
+
+    <div v-else @click="handleCartClick" class="w-full cursor-pointer">
+      <div
+        class="w-full flex items-center justify-between border-b-2 sm:mt-1 sm:border-0 py-2"
+      >
+        <template v-if="shouldShowAuthNavItems">
+          {{ props.title }}
+        </template>
+        <font-awesome-icon :icon="props.icon" v-if="screenWidth < 640" />
+      </div>
+    </div>
     <p
       v-if="props.title === 'Cart' && shouldShowNavItems"
       class="hidden ml-1 bg-green-600 text-white rounded-full p-2 w-6 h-6 sm:flex items-center justify-center text-xs font-semibold"
