7c7e0a402fef34406923c6566f830e312b6ceec2
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-06 10:24:40 +0100

    fixed styles

--- components/cards/MealCard/MealCard.vue
+++ components/cards/MealCard/MealCard.vue
@@ -23,9 +23,11 @@ const mealItem = {
 </script>
 
 <template>
-  <article class="flex shadow-lg p-6 flex-col relative cursor-pointer rounded-md">
+  <article
+    class="flex flex-col shadow-lg p-4 sm:p-6 relative cursor-pointer rounded-md bg-white w-full sm:max-w-[400px] md:max-w-[500px]"
+  >
     <div class="relative">
-      <div class="relative overflow-hidden w-full max-w-[500px]">
+      <div class="relative overflow-hidden w-full h-[200px] sm:h-[250px]">
         <NuxtImg
           :src="props.meal.recipe.image"
           :alt="replaceRecipeText(props.meal.recipe.label)"
@@ -37,32 +39,35 @@ const mealItem = {
         />
       </div>
       <p
-        class="absolute bottom-2 left-2 px-2 py-1 text-sm font-semibold tracking-wide text-gray-100 bg-opacity-50 rounded-md"
+        class="absolute bottom-2 left-2 px-2 py-1 text-xs sm:text-sm font-semibold tracking-wide text-gray-100 bg-opacity-50 rounded-md"
         :style="{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }"
       >
         {{ capitalizeFirstLetter(props.meal.recipe.cuisineType[0]) }}
       </p>
     </div>
-    <div class="flex flex-col h-full">
-      <h3 class="font-bold text-sm mt-1 flex-1">
+    <div class="flex flex-col h-full mt-2 sm:mt-4">
+      <h3 class="font-bold text-sm sm:text-base mt-1 flex-1 truncate">
         {{ replaceRecipeText(props.meal.recipe.label) }}
       </h3>
 
-      <div class="relative flex justify-between items-center mt-4">
+      <div class="relative flex justify-between items-center mt-4 sm:mt-5">
         <div class="flex items-center gap-2 py-1 px-3 bg-amber-400 rounded-md">
           <font-awesome-icon
             v-if="isLoaded"
             data-test="fa-icon"
             :icon="['fas', 'fa-truck']"
-            class="text-xs"
+            class="text-xs sm:text-sm"
           />
-          <p class="text-xs font-semibold">Free</p>
+          <p class="text-xs sm:text-sm font-semibold">Free</p>
         </div>
         <div class="absolute right-0 z-50">
           <HeartBtn :mealItem="mealItem" />
         </div>
       </div>
-      <p data-test="price">{{ props.price }} $</p>
+
+      <div class="flex justify-between mt-2 sm:mt-3">
+        <p data-test="price" class="font-bold text-lg sm:text-xl">{{ props.price }} $</p>
+      </div>
     </div>
   </article>
 </template>
