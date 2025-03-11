755372135ca3df5fb91cfa052c4e43fddf816ec6
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-11 13:33:27 +0100

    added footer data

---
+++ data/footerData.ts
@@ -0,0 +1,26 @@
+export const aboutData = [
+  { id: 'about', title: 'About Glovo', links: ['About Us', 'Careers', 'Press', 'Blog'] },
+  {
+    id: 'services',
+    title: 'Services',
+    links: ['Food Delivery', 'Groceries', 'Pharmacy', 'Courier'],
+  },
+  {
+    id: 'legal',
+    title: 'Legal',
+    links: ['Terms & Conditions', 'Privacy Policy', 'Cookie Policy'],
+  },
+  { id: 'download', title: 'Download the App', links: ['App Store', 'Google Play'] },
+];
+
+export const socialLinks = [
+  { id: 'facebook', icon: ['fab', 'facebook'] },
+  { id: 'twitter', icon: ['fab', 'twitter'] },
+  { id: 'instagram', icon: ['fab', 'instagram'] },
+  { id: 'linkedin', icon: ['fab', 'linkedin'] },
+];
+
+export const appDownloadIcons = [
+  { id: 'app-store', icon: ['fab', 'app-store'] },
+  { id: 'google-play', icon: ['fab', 'google-play'] },
+];
