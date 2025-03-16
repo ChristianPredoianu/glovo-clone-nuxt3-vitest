d42860c7e9c9e6bd33a6fc10c130dc47e3711c46
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-16 10:00:04 +0100

    fixet heading size

--- components/modals/ProductModal/Overlay/ProductModalOverlay.vue
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.vue
@@ -35,39 +35,49 @@ function isMealModalProps(props: ModalProps | null): props is IMealModalProps {
 <template>
   <article
     v-if="productModalProps"
-    class="container h-full relative mx-auto p-10 flex flex-col flex-grow overflow-auto"
+    class="container h-full mx-auto p-6 md:p-10 flex flex-col flex-grow overflow-auto"
   >
-    <div class="mx-auto relative overflow-hidden w-full max-w-[500px] aspect-[16/9]">
+    <div
+      class="mx-auto relative overflow-hidden w-full max-w-[500px] aspect-[16/9] rounded-lg shadow-lg flex-shrink-0"
+    >
       <img
         :src="props.productModalProps!.img"
         :alt="props.productModalProps!.label"
-        class="rounded-lg object-cover w-full h-full"
+        class="object-cover w-full h-full"
       />
     </div>
 
-    <h3 class="text-xl font-semibold text-gray-700 py-4">
-      {{ replaceRecipeText(props.productModalProps!.label) }}
-    </h3>
-    <p class="mb-4 text-lg font-semibold text-green-800">
-      {{ productModalProps.price }} $
-    </p>
-    <div
-      v-if="isMealModalProps(props.productModalProps)"
-      class="mt-2 mb-8 gap-2 text-gray-500"
-    >
-      <ProductModalList :ingredients="props.productModalProps!.ingredients" />
-    </div>
-    <CtaBtn
-      :textCol="'text-gray-200'"
-      @click="handleAddToCart(product as IMealModalProps)"
-      >Add to cart {{ props.productModalProps!.price }} $</CtaBtn
-    >
-    <div class="h-4">
-      <transition name="fade" mode="out-in">
-        <div class="mt-4 w-full overflow-hidden" v-show="isBarActive">
-          <ProgressBar :progressWidth="progressWidth" />
-        </div>
-      </transition>
+    <div class="mt-6 text-center md:text-left flex flex-col flex-grow">
+      <h3 class="text-xl md:text-2xl font-bold text-gray-800 mb-4">
+        {{ replaceRecipeText(props.productModalProps!.label) }}
+      </h3>
+      <p class="text-xl md:text-2xl font-semibold text-green-600 mb-6">
+        {{ productModalProps.price }} $
+      </p>
+
+      <div
+        v-if="isMealModalProps(props.productModalProps)"
+        class="mt-4 mb-8 text-gray-600 flex-grow"
+      >
+        <ProductModalList :ingredients="props.productModalProps!.ingredients" />
+      </div>
+
+      <div class="flex-shrink-0">
+        <CtaBtn
+          class="w-full md:w-auto px-8 py-3 bg-green-600 hover:bg-green-700 text-white font-semibold rounded-lg transition-colors duration-300"
+          @click="handleAddToCart(product as IMealModalProps)"
+        >
+          Add to cart {{ props.productModalProps!.price }} $
+        </CtaBtn>
+      </div>
+
+      <div class="h-4 mt-6 flex-shrink-0">
+        <transition name="fade" mode="out-in">
+          <div class="w-full overflow-hidden" v-show="isBarActive">
+            <ProgressBar :progressWidth="progressWidth" />
+          </div>
+        </transition>
+      </div>
     </div>
   </article>
 </template>
