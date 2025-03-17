573a49078d811fd7682338b70267b053bd99bfc4
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-17 18:56:37 +0100

    added nuxt link

--- components/header/Logo.vue
+++ components/header/Logo.vue
@@ -1,3 +1,5 @@
 <template>
-  <span class="font-semibold text-green-500 text-xl">GlovoApp</span>
+  <NuxtLink to="/">
+    <span class="font-semibold text-green-500 text-xl cursor-pointer">GlovoApp</span>
+  </NuxtLink>
 </template>
