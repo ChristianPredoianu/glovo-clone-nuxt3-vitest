b1375b7e0e203759d99cc4b6f905107ee26b9188
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-18 09:02:53 +0100

    changed collor

--- components/header/Logo.vue
+++ components/header/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLink to="/">
-    <span class="font-semibold text-green-600 text-xl cursor-pointer">GlovoApp</span>
+    <span class="font-semibold text-green-700 text-xl cursor-pointer">GlovoApp</span>
   </NuxtLink>
 </template>
