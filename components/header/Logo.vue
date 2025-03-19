96df00ce733e52096e097e2e685da2f4221b669d
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-19 12:56:18 +0100

    work

--- components/header/Logo.vue
+++ components/header/Logo.vue
@@ -1,5 +1,7 @@
 <template>
   <NuxtLink to="/">
-    <span class="font-semibold text-green-700 text-xl cursor-pointer">GlovoApp</span>
+    <span data-test="logo" class="font-semibold text-green-700 text-xl cursor-pointer"
+      >GlovoApp</span
+    >
   </NuxtLink>
 </template>
